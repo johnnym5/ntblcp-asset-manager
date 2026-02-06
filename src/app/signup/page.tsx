9440292197b8d124162b8ff5418cd3c5fdde68c2
Your Name <jegbase@gmail.com>
2026-02-06 15:13:30 +0000

    make dark mode amoled black and royal gold assent, then start scan for e

--- src/app/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-// This page has been removed as user signups are no longer supported.
-// Accounts must be created by an administrator through the settings panel.
-export default function SignupPage() {
-  return null;
-}
--- src/lib/data.ts
+++
@@ -1,5 +0,0 @@
-import type { Asset } from './types';
-
-// This file is intentionally left blank. 
-// It was previously used for sample data during development.
-export const sampleAssets: Asset[] = [];
