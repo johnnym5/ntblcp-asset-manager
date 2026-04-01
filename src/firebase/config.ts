4a474b10bfb7fd79754bd3ca434eda5d932b62ee
Egbase John Mary <jegbase@gmail.com>
2026-04-01 11:15:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ntblcp-asset-manager-k7hy1",
+  "appId": "1:45027293772:web:f96a04d54ebc503c4c52e9",
+  "apiKey": "AIzaSyB9O662zXyyJkhtle1snkNLapre3_Sa3rc",
+  "authDomain": "ntblcp-asset-manager-k7hy1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "45027293772"
+};
