101632551e48f1201634eb55efc80f3b3ff01bf7
Egbase John Mary <jegbase@gmail.com>
2026-04-07 17:33:22 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "ntblcp-asset-manager-k7hy1",
-  "appId": "1:45027293772:web:f96a04d54ebc503c4c52e9",
-  "apiKey": "AIzaSyB9O662zXyyJkhtle1snkNLapre3_Sa3rc",
-  "authDomain": "ntblcp-asset-manager-k7hy1.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "45027293772",
-  "storageBucket": "ntblcp-asset-manager-k7hy1.appspot.com",
-  "databaseURL": "https://ntblcp-asset-manager-k7hy1-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyDhBbeiAZGWzCHQXbbTcNXdsIT_6Gz3qJU",
+  authDomain: "ntblcp-asset-manager.firebaseapp.com",
+  databaseURL: "https://ntblcp-asset-manager-default-rtdb.firebaseio.com",
+  projectId: "ntblcp-asset-manager",
+  storageBucket: "ntblcp-asset-manager.firebasestorage.app",
+  messagingSenderId: "573481845773",
+  appId: "1:573481845773:web:5040ae77a51681c05bc144"
 };
