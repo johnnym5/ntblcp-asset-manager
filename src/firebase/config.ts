6de0a53b8a31c5c3caeccf44e98783f2b94388d3
Egbase John Mary <jegbase@gmail.com>
2026-04-07 18:31:22 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDhBbeiAZGWzCHQXbbTcNXdsIT_6Gz3qJU",
-  authDomain: "ntblcp-asset-manager.firebaseapp.com",
-  databaseURL: "https://ntblcp-asset-manager-default-rtdb.firebaseio.com",
-  projectId: "ntblcp-asset-manager",
-  storageBucket: "ntblcp-asset-manager.firebasestorage.app",
-  messagingSenderId: "573481845773",
-  appId: "1:573481845773:web:5040ae77a51681c05bc144"
+  "projectId": "ntblcp-asset-manager",
+  "appId": "1:573481845773:web:4b458fcc2829fc135bc144",
+  "apiKey": "AIzaSyCi3yzw1S_Lci4aSA-FcmOCA-rLtFW4nos",
+  "authDomain": "ntblcp-asset-manager.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "573481845773"
 };
